7d6382c88d5f7f494bfa44b76c57eca38c2c7684
Mark Harvey <markedwinharvey@gmail.com>
2016-01-10 01:59:25 -0800

    add readme and update perm.py

--- perm.py
+++ perm.py
@@ -3,6 +3,6 @@ import subprocess,time
 def main():
 	subprocess.Popen(['python ./server.py &'],shell=True)#check for running server/start server
 	time.sleep(1)
-	subprocess.Popen(['open -a firefox http://localhost:8000/perm'],shell=True)
+	subprocess.Popen(['open http://localhost:8000/perm'],shell=True)
 if __name__ == '__main__':
 	main()
