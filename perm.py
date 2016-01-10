41c7c3d328273d8a7cdfcf0372b5e82548eac6cc
Mark Harvey <markedwinharvey@gmail.com>
2016-01-10 01:47:27 -0800

    second

--- perm.py
+++ perm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import subprocess,time
 def main():
-	subprocess.Popen(['python ~/p/server.py &'],shell=True)#check for running server/start server
+	subprocess.Popen(['python ./server.py &'],shell=True)#check for running server/start server
 	time.sleep(1)
 	subprocess.Popen(['open -a firefox http://localhost:8000/perm'],shell=True)
 if __name__ == '__main__':
---
+++ server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import BaseHTTPServer,CGIHTTPServer,cgitb,subprocess,sys,requests
+try:
+	a = requests.get('http://localhost:8000')
+	print '*** server running ***'
+	print '*** the following information is available:'
+	subprocess.Popen(['ps -fA | grep server.py'],shell=True)
+except:
+	print '*** server not running ***';print '*** starting server ***'
+	cgitb.enable()  ## This line enables CGI error reporting
+ 
+	server = BaseHTTPServer.HTTPServer
+	handler = CGIHTTPServer.CGIHTTPRequestHandler
+	server_address = ("", 8000)
+	handler.cgi_directories = ["/"]
+ 
+	httpd = server(server_address, handler)
+	httpd.serve_forever()
